657d8a41ba1fbb484a8e7ede73f0ce40ca95ce42
Mukesh Rajbanshi <59588637+mukesh-890@users.noreply.github.com>
2022-06-06 21:02:29 +0530

    initial commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
+        <>
+        <NavLink className = "index-link" to = "/"></nav>
+        <NavLink className = "home-link" to ="/home"></nav>
+        
+        </>
 
     )
 }
