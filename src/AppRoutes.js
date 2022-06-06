64e1022623947baab80166691475b1db0114aa4a
Mukesh Rajbanshi <59588637+mukesh-890@users.noreply.github.com>
2022-06-06 21:19:01 +0530

    initial commit

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {Route} from 'react-router';
 
 export const AppRoutes = () => {
     return (
+        <>
+        <Route  path = "/"  component = {Index} />
+        <Route path ="/home" component = {Home} />
+        </>
 
     )
 }
