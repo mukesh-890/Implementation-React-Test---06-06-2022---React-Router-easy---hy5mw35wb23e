295f916d80bca26161fbf47fa53786694e95e759
Mukesh Rajbanshi <59588637+mukesh-890@users.noreply.github.com>
2022-06-06 21:32:57 +0530

    initial comnit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink ,Link} from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
-        <>
-        <NavLink className = "index-link" to = "/"></nav>
-        <NavLink className = "home-link" to ="/home"></nav>
-        
-        </>
+        <NavLink>
+         <nav>
+          <ul>
+            <li>
+              <Link to="/" className = "index-link">Index</Link>
+            </li>
+            <li>
+              <Link to="/home" className = "home-link">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        </NavLink>
 
     )
 }
